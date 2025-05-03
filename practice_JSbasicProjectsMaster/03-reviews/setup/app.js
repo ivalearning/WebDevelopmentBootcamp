eaa19e0386fe6e3d46528e25d73ae5c34f1b9f68
ivalearning <irena_vanickova@centrum.cz>
2025-05-03 10:30:45 +0200

    reviews completed

--- practice_JSbasicProjectsMaster/03-reviews/setup/app.js
+++ practice_JSbasicProjectsMaster/03-reviews/setup/app.js
@@ -44,16 +44,12 @@ let currentItem = 0;
 
 // load initial item
 window.addEventListener('DOMContentLoaded', function () {
-  const item = reviews[currentItem];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
+showPerson(currentItem);
 });
 
 // show person based on item
-function showPerson(person) {
-  const item = reviews[person];
+function showPerson() {
+  const item = reviews[currentItem];
   img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
@@ -65,7 +61,7 @@ nextBtn.addEventListener('click', function () {
   if (currentItem > reviews.length - 1) {
     currentItem = 0;
   }
-  showPerson(currentItem);
+  showPerson();
 });
 // show prev person
 prevBtn.addEventListener('click', function () {
@@ -73,12 +69,12 @@ prevBtn.addEventListener('click', function () {
   if (currentItem < 0) {
     currentItem = reviews.length - 1;
   }
-  showPerson(currentItem);
+  showPerson();
 });
 // show random person
 randomBtn.addEventListener('click', function () {
   console.log('hello');
 
   currentItem = Math.floor(Math.random() * reviews.length);
-  showPerson(currentItem);
+  showPerson();
 });
