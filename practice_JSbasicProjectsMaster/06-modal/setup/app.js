324e57a5b490ea0b02d48079bae20c8e6e520f85
ivalearning <irena_vanickova@centrum.cz>
2025-05-16 14:19:01 +0200

    6 completed

--- practice_JSbasicProjectsMaster/06-modal/setup/app.js
+++ practice_JSbasicProjectsMaster/06-modal/setup/app.js
@@ -2,3 +2,23 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+
+const modalButton = document.querySelector('.modal-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeButton = document.querySelector('.close-btn');
+const modal = document.querySelector('.modal-container');
+
+modalButton.addEventListener('click', function() {
+
+    modalOverlay.classList.add('open-modal');
+    modal.classList.add('modal-bigger');
+    //    console.log(modalOverlay.classList);
+});
+
+closeButton.addEventListener('click', function() {
+    modalOverlay.classList.remove('open-modal');
+    modal.classList.remove('modal-bigger');
+    //console.log(modalOverlay.classList);
+
+});
