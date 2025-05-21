3fbf2701f88b373196dfb4487dabc3f0651bfce5
ivalearning <irena_vanickova@centrum.cz>
2025-05-21 15:55:40 +0200

    add, delete the kist item added

--- practice_JSbasicProjectsMaster/14-grocery-bud/setup/app.js
+++ practice_JSbasicProjectsMaster/14-grocery-bud/setup/app.js
@@ -14,7 +14,8 @@ let editId = "";
 
 // ****** EVENT LISTENERS **********
 //submit form
-form.addEventListener('submit', addItem)
+form.addEventListener('submit', addItem);
+clearBtn.addEventListener('click', clearList);
 
 // ****** FUNCTIONS **********
 
@@ -24,11 +25,11 @@ function addItem(e) {
 
    const input = inputItem.value;
    const id = new Date().getTime().toString();
-   console.log(id);
+   //console.log(id);
    
    if (input !== '' && editFlag === false) {        // da se zkratit na if (input && !editFlag), protoze pokud se zada prazdny retezec, tak bude input false
     //pokud kontroluji false, tak pridam !pred podminku
-    console.log('novy zaznam');
+    //console.log('novy zaznam');
 
         // vytvoreni article elementu
     const newItem = document.createElement('article');
@@ -54,6 +55,12 @@ function addItem(e) {
               </button>
             </div> `;
     
+    const deleteBtn = newItem.querySelector('.delete-btn');
+    const editBtn = newItem.querySelector('.edit-btn');
+
+    deleteBtn.addEventListener('click', deleteItem);
+    editBtn.addEventListener('click', editItem)
+    
     // append child
     list.appendChild(newItem);
 
@@ -63,6 +70,10 @@ function addItem(e) {
     // show container
     listContainer.classList.add('show-container');
    
+
+    // add to local storage
+    addToLocalStorage(id, newItem);                                                 // doplnit
+    setBackToDefault(  );
 
        }
    else if (input !== '' && editFlag === true) {    // zkracene if (input && editFlag)
@@ -92,6 +103,71 @@ function displayAlert(msg, color) {
     }, 1000)
 }
 
+function addToLocalStorage(id,value) {
+    console.log('saved');
+    
+
+};
+
+function setBackToDefault() {
+    console.log('default');
+    inputItem.value = "";
+    editFlag = false;
+    editId = "";
+    submitBtn.textContent = 'Add'
+}
+
+function clearList() {
+    const items = document.querySelectorAll('.grocery-item');
+
+    if (items.length > 0) {
+        items.forEach( function(i) {
+            list.removeChild(i);
+    });
+    }
+     listContainer.classList.remove('show-container');
+     displayAlert('list cleared', 'danger');
+     setBackToDefault();
+
+     //local storage remove items
+}
+// smazani radku pomoci ikonky kose
+function deleteItem(e) {
+    const element = e.currentTarget.parentElement.parentElement;
+    const id = element.dataset.id                                               //   ID
+
+
+    list.removeChild(element);
+
+    //pokud po smazani nezbyva zadna polozka, tak i 
+    if (list.children.length === 0) {
+        listContainer.classList.remove('show-container');
+    }
+    displayAlert('item removed', 'danger');
+    setBackToDefault();
+    //local storage remove items                        // doplnit
+};
+
+// editace jmena polozky pomoci ikonky edit
+function editItem() {
+    const element = e.currentTarget.parentElement.parentElement;
+
+};
+
+
+
+
+
 // ****** LOCAL STORAGE **********
+
+function addToLocalStorage(id, value) {
+
+}
+
+function removeFromLocalStorage(id) {
+
+}
+
+
 
 // ****** SETUP ITEMS **********
