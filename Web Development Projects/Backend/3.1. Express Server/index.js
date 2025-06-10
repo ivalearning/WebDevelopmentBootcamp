271adc04a388bc285fbce0497d0f02a8d899777d
ivalearning <irena_vanickova@centrum.cz>
2025-06-10 18:03:55 +0200

    sec 24 - 201+202

---
+++ Web Development Projects/Backend/3.1. Express Server/index.js
@@ -0,0 +1,18 @@
+import express  from "express";
+
+const app = express();                          // 
+
+/*
+app.listen(3000, () => {                        // 3000 cislo portu
+    console.log("Listening on port 3000");      // callback funkce
+})
+
+*/
+
+// lepsi je cislo portu vzdefinovat jako konstantu
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}.`);
+})
