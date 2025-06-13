73ee78fd2c05a8c46f64b2604b98b2e250be94e5
ivalearning <irena_vanickova@centrum.cz>
2025-06-13 17:28:03 +0200

    s206 completed

--- Web Development Projects/Backend/3.4+Middleware/index2.js
+++ Web Development Projects/Backend/3.4+Middleware/index2.js
@@ -1,7 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
+
+app.use(morgan("short")); // https://www.npmjs.com/package/morgan
 
 app.get("/", (req, res) => {
   res.send("Hello");
--- Web Development Projects/Backend/3.4+Middleware/index4.js
+++ Web Development Projects/Backend/3.4+Middleware/index4.js
@@ -1,7 +1,25 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  
+ // console.log("body", req.body.street+req.body.pet);
+
+ res.send(`<h1>Your band name is: </h1> 
+  <h2>${req.body.street+req.body.pet} 🤘</h2>`);
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
