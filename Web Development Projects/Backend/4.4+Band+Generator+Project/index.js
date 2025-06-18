f9dcbbc575f1093ceb487447d412b7a800dcdd2d
ivalearning <irena_vanickova@centrum.cz>
2025-06-18 18:56:53 +0200

    sec 25 completed

--- Web Development Projects/Backend/4.4+Band+Generator+Project/index.js
+++ Web Development Projects/Backend/4.4+Band+Generator+Project/index.js
@@ -1,37 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-const app = express();
-const port = 3000;
-
-//Step 3 - Make the styling show up.
-//Hint 1: CSS files are static files!
-//Hint 2: The header and footer are partials.
-//Hint 3: Add the CSS link in header.ejs
-
-//Step 4 - Add a dynamic year to the footer.
-//Hint: Google to find out how to get the current year using JS.
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-  //Step 1 - Make the get route work and render the index.ejs file.
-});
-
-app.post("/submit", (req, res) => {
-  //Step 2 - Make the generate name functionality work
-  //Hint: When the "Generate Name" button in index.ejs is clicked, it should hit up this route.
-  //Then:
-  //1. You should randomly pick an adjective from the const "adj" and a noun from const "noun",
-  //scroll down to see the two arrays.
-  //2. Send the index.ejs as a response and add the adjective and noun to the res.render
-  //3. Test to make sure that the random words display in the h1 element in index.ejs
-});
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
-
 const adj = [
   "abandoned",
   "able",
@@ -5692,3 +5661,61 @@ const noun = [
   "zoot-suit",
   "zucchini",
 ];
+
+const app = express();
+const port = 3000;
+
+//Step 3 - Make the styling show up.
+//Hint 1: CSS files are static files!
+//Hint 2: The header and footer are partials.
+//Hint 3: Add the CSS link in header.ejs
+
+//Step 4 - Add a dynamic year to the footer.
+//Hint: Google to find out how to get the current year using JS.
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Step 1 - Make the get route work and render the index.ejs file.
+app.get("/", (req, res) => {
+  const header = "Hello"
+    res.render("index.ejs", {
+      header:header
+    }
+    )
+});
+
+app.use(express.static("public"));
+
+//Step 2 - Make the generate name functionality work
+  //Hint: When the "Generate Name" button in index.ejs is clicked, it should hit up this route.
+  //Then:
+  //1. You should randomly pick an adjective from the const "adj" and a noun from const "noun",
+  //scroll down to see the two arrays.
+  //2. Send the index.ejs as a response and add the adjective and noun to the res.render
+  //3. Test to make sure that the random words display in the h1 element in index.ejs
+
+app.post("/submit", (req, res) => {
+  const randomNrAdj = Math.floor((Math.random() * adj.length));
+  const randomNrNoun = Math.floor((Math.random() * noun.length));
+  let adjective = adj[randomNrAdj];
+  let name = noun[randomNrNoun];
+  let header = adjective + " " + name
+  
+  console.log(header);
+  
+  res.render("index.ejs", 
+    {
+     word1: adjective,
+     word2: name,
+    }
+  )  
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+/* <h1> <%= header %> </h1>
+    */
+
+
