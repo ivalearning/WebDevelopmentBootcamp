f72d6daae1c3adcd543bdba0387d49a9aa96a13a
ivalearning <irena_vanickova@centrum.cz>
2025-09-18 13:57:31 +0200

    s299 completed

---
+++ Web Development Projects/React/smileys-app/src/components/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import emojipedia from "../emojipedia";
+import Card from "./Card"
+
+function emojiCard(emoji) {
+  return (
+    <Card 
+      key={emoji.id}
+      emoji={emoji.emoji}
+      name={emoji.name}
+      meaning={emoji.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+        {emojipedia.map(emojiCard)}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
